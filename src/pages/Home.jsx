375961a626adf7747883a7affd4b9e7f3480b683
Kleriz171 <bibakleris@gmail.com>
2025-11-15 16:03:52 +0100

    Added shit ton stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackround } from "../components/StarBackround"
+import { Navbar } from "../components/Navbar"
 
 export const Home = () =>{
     return(
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             <ThemeToggle/>
+            <StarBackround />
+            <Navbar/>
         </div>
     )
 }
