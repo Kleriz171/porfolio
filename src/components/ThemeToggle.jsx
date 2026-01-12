ee68133ffbe84e931617c2b33f14004c73890b98
Kleriz171 <bibakleris@gmail.com>
2026-01-12 20:56:40 +0100

    Dark mode default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,38 +5,39 @@ import { cn } from "@/lib/utils"
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(true)
 
-    useEffect(() =>{
+    useEffect(() => {
         const storedTheme = localStorage.getItem("theme")
-        if(storedTheme === "dark"){
+        if (storedTheme === "light") {
+            setIsDarkMode(false)
+            document.documentElement.classList.remove("dark")
+        }
+        else {
             setIsDarkMode(true)
             document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark")
         }
-        else{
-            setIsDarkMode(false)
-            document.documentElement.classList.remove("dark")
-        }
-    },[])
+    }, [])
 
-    const toggleTheme = () =>{
-        if (isDarkMode){
+    const toggleTheme = () => {
+        if (isDarkMode) {
             document.documentElement.classList.remove("dark")
-                        localStorage.setItem("theme", "light")
+            localStorage.setItem("theme", "light")
             setIsDarkMode(false)
-        }else{
+        } else {
             document.documentElement.classList.add("dark")
             localStorage.setItem("theme", "dark")
             setIsDarkMode(true)
         }
     }
 
-    return( 
-    <button onClick={toggleTheme}
-    className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outline-hidden"
-    )}
-    >{isDarkMode ? 
-    <Sun className="h-6 w-6 text-yellow-300"/> : 
-    <Moon className="h-6 w-6 text-blue-900"/>}
-    </button>
+    return (
+        <button onClick={toggleTheme}
+            className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+                "focus:outline-hidden"
+            )}
+        >{isDarkMode ?
+            <Sun className="h-6 w-6 text-yellow-300" /> :
+            <Moon className="h-6 w-6 text-blue-900" />}
+        </button>
     )
 }
