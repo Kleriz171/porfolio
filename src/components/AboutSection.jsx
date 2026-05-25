36d8bb2c37ac1a84ca63b33c3eadd7c22d002de0
Kleriz171 <bibakleris@gmail.com>
2026-05-25 22:25:25 +0200

    lala

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -23,7 +23,7 @@ export const AboutSection = () =>{
                         <a href="#contact" className="cosmic-button">
                             Get In Touch
                         </a>
-                        <a href="/" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                        <a href="/Kleris_Biba_CV_1.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                             Download CV
                         </a>
                     </div>
